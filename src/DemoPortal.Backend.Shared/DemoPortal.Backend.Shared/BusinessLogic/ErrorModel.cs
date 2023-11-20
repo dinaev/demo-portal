59d1c62e13d6de03a28b2ba62ec36036f8ed0f21
Azamat Dinaev <azamat.dinaev@gmail.com>
2023-11-20 13:12:16 +0300

    Shared Library - Add BusinessResult and ErrorModel

---
+++ src/DemoPortal.Backend.Shared/DemoPortal.Backend.Shared/BusinessLogic/ErrorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoPortal.Backend.Shared.BusinessLogic
+{
+    public class ErrorModel
+    {
+        public ErrorModel(string key, string message)
+        {
+            Key = key;
+            Message = message;
+        }
+
+        public string Key { get; }
+        public string Message { get; }
+    }
+}
