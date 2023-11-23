e1ff092e696ecb315f8afed3454ac2cc24e49c3a
Azamat Dinaev <azamat.dinaev@gmail.com>
2023-11-23 20:06:49 +0300

    Documents API - Add readme and error keys to API client

--- src/DemoPortal.Backend.Documents/DemoPortal.Backend.Documents.Abstractions/Errors/DocumentsErrorModelKeys.cs
+++ src/DemoPortal.Backend.Documents/DemoPortal.Backend.Documents.Abstractions/Errors/DocumentsErrorModelKeys.cs
@@ -1,14 +1,52 @@
 ﻿namespace DemoPortal.Backend.Documents.Abstractions.Errors;
 
+/// <summary>
+/// Document API error codes
+/// </summary>
 public class DocumentsErrorModelKeys
 {
+    /// <summary>
+    /// Document not found
+    /// </summary>
     public const string DocumentNotFound = "documents/not-found";
+    
+    /// <summary>
+    /// Document ID not provided
+    /// </summary>
     public const string DocumentIdNotProvided = "documents/id-not-provided";
+    
+    /// <summary>
+    /// Error occured during document updating
+    /// </summary>
     public const string DocumentUpdatingError = "documents/updating-error";
+    
+    /// <summary>
+    /// Error occured during document deletion
+    /// </summary>
     public const string DocumentDeletionError = "documents/deletion-error";
+    
+    /// <summary>
+    /// User (ID) not provided
+    /// </summary>
     public const string UserNotProvided = "documents/user-not-provided";
+    
+    /// <summary>
+    /// Document title is empty
+    /// </summary>
     public const string TitleIsEmpty = "documents/empty-title";
+    
+    /// <summary>
+    /// Document title is too long
+    /// </summary>
     public const string TitleIsTooLong = "documents/title-too-long";
+    
+    /// <summary>
+    /// Document text is empty
+    /// </summary>
     public const string TextIsEmpty = "documents/empty-text";
+    
+    /// <summary>
+    /// Document text is too long
+    /// </summary>
     public const string TextIsTooLong = "documents/text-too-long";
 }
